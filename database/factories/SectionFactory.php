36e057b7b65bde7f5c2b0b55b6a1aa2d2ed001cc
Felipe Balloni Ferreira <felipe.balloni@hotmail.com>
2024-11-30 14:17:52 -0300

    [up] Update factories to namespace classes

--- database/factories/SectionFactory.php
+++ database/factories/SectionFactory.php
@@ -1,25 +1,23 @@
 <?php
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
+namespace Database\Factories;
 
-use App\Models\Section;
+use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
-use Faker\Generator as Faker;
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| This directory should contain each of the model factory definitions for
-| your application. Factories provide a convenient way to generate new
-| model instances for testing / seeding your application's database.
-|
-*/
+class SectionFactory extends Factory
+{
 
-$factory->define(Section::class, function (Faker $faker) {
-    return [
-        'text' => Str::title($faker->sentence(20)),
-        'type' => 'text',
-    ];
-});
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition(): array
+    {
+        return [
+            'text' => Str::title($this->faker->sentence(20)),
+            'type' => 'text',
+        ];
+    }
+}
